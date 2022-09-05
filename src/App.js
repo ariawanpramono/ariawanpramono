bd7c8918bb7a0412d2d4d5a1a4b5fbac47c8778e
ariawanpramono <106358615+ariawanpramono@users.noreply.github.com>
2022-09-05 18:48:10 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function Typhography() {
+  return (
+    <div className="Typhography">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Hello CodeSandbox</h2>
+      <h3>Hello CodeSandbox</h3>
+      <h4>Hello CodeSandbox</h4>
+      <h5>Hello CodeSandbox</h5>
+      <h6>Hello CodeSandbox</h6>
+    </div>
+  );
+}
